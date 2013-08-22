fd3e92084066473f1e9e691410a36802692d54e2
conejoninja <conejo@theninjabunny.com>
2013-08-22 09:09:48 +0200

    improvements

--- test.php
+++ test.php
@@ -6,4 +6,12 @@ $coinjar = new CoinJar('YOUR CHECKOUT USER', 'YOUR CHECKOUT PASSWORD', 'YOUR API
 $account = $coinjar->accountInformation();
 print_r($account);
 
+$items[0]['name'] = 'Name';
+$items[0]['quantity'] = 1;
+$items[0]['amount'] = 2;
+$order = $coinjar->createOrder($items, 'usd', 'invoice#1', 'coinjar-php', 'notify-url', 'retrn-url', 'cancel-url');
+print_r($order);
+
+?>
+
 ?>
